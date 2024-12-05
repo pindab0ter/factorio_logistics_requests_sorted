c6ca570a2d506a0a533711538b0ff15aacfa6370
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-12-05 20:25:22 +0100

    Proof of concept for Factorio 2.0

--- control.lua
+++ control.lua
@@ -1,70 +1,78 @@
-local Inventory = require("scripts/inventory")
+local inventory = require("scripts/inventory")
 
-local function SyncButton(player)
-    player.set_shortcut_toggled("logistics_requests_sorted-enabled", global.sorting_enabled[player.index])
+local function update_button(player)
+    player.set_shortcut_toggled("logistics_requests_sorted-enabled", storage.sorting_enabled[player.index])
 end
 
-local function ApplySorting(player, entity)
-    if global.sorting_enabled[player.index] then
-        Inventory.SortEntityInventory(entity)
+--- @param player LuaPlayer
+--- @param entity LuaEntity
+local function apply_sorting(player, entity)
+    if storage.sorting_enabled[player.index] then
+        inventory.sort_logistic_sections(entity)
     end
 end
 
-
--- player management
-local function InitPlayer(player)
-    global.sorting_enabled[player.index] = true
-    SyncButton(player)
-    -- apply to main inventory right away
-    ApplySorting(player, player.character)
+--- @param player LuaPlayer
+local function initialize_player(player)
+    storage.sorting_enabled[player.index] = true
+    update_button(player)
+    apply_sorting(player, player.character)
 end
 
-local function DelPlayer(player_index)
-    global.sorting_enabled[player_index] = nil
+--- @param event EventData.on_player_created
+local function on_player_created(event)
+    local player = game.players[event.player_index] or error("Player " .. event.player_index .. " not found")
+    initialize_player(player)
 end
 
-local function OnConfigurationChanged()
-    if global.sorting_enabled == nil then
-        global.sorting_enabled = {}
+--- @param event EventData.on_player_removed
+local function on_player_removed(event)
+    storage.sorting_enabled[event.player_index] = nil
+end
+
+local function on_configuration_changed()
+    if storage.sorting_enabled == nil then
+        storage.sorting_enabled = {}
     end
-    -- enable if not configured yet
-    for index, player in pairs(game.players) do
-        if global.sorting_enabled[player.index] == nil then
-            InitPlayer(player)
+
+    for _, player in pairs(game.players) do
+        if storage.sorting_enabled[player.index] == nil then
+            initialize_player(player)
         end
     end
 end
 
-
--- runtime management
-local function OnSlotsChanged(event)
-    -- only if player changed it directly
-    if event.player_index ~= nil then
-        ApplySorting(game.get_player(event.player_index), event.entity)
-    end
-end
-
-local function OnShortcut(player_index)
-    local player = game.get_player(player_index)
-    global.sorting_enabled[player.index] = not global.sorting_enabled[player.index]
-    SyncButton(player)
-    -- apply to main inventory right away
-    ApplySorting(player, player.character)
-end
-
-local function OnLuaShortcut(event)
-    -- only compute our own event
-    if event.prototype_name ~= "logistics_requests_sorted-enabled" then
+--- @param event EventData.on_entity_logistic_slot_changed
+local function on_entity_logistic_slot_changed(event)
+    -- Only if player changed it directly
+    if event.player_index == nil then
         return
     end
-    OnShortcut(event.player_index)
+
+    local player = game.get_player(event.player_index) or error("Player " .. event.player_index .. " not found")
+
+    apply_sorting(player, event.entity)
 end
 
+--- @param event EventData.on_lua_shortcut|EventData.CustomInputEvent
+local function on_lua_shortcut(event)
+    if event.prototype_name ~= "logistics-requests-sorted-enabled" then
+        return
+    end
 
-script.on_init(OnConfigurationChanged)
-script.on_configuration_changed(OnConfigurationChanged)
-script.on_event(defines.events.on_player_created, function(event) InitPlayer(game.get_player(event.player_index)) end)
-script.on_event(defines.events.on_player_removed, function(event) DelPlayer(event.player_index) end)
-script.on_event(defines.events.on_entity_logistic_slot_changed, OnSlotsChanged)
-script.on_event(defines.events.on_lua_shortcut, OnLuaShortcut)
-script.on_event("logistics_requests_sorted-hotkey", function(event) OnShortcut(event.player_index) end)
+    local player = game.get_player(event.player_index) or error("Player " .. event.player_index .. " not found")
+
+    storage.sorting_enabled[player.index] = not storage.sorting_enabled[player.index]
+
+    update_button(player)
+
+    apply_sorting(player, player.character)
+end
+
+script.on_init(on_configuration_changed)
+script.on_configuration_changed(on_configuration_changed)
+script.on_event(defines.events.on_player_created, on_player_created)
+script.on_event(defines.events.on_player_removed, on_player_removed)
+script.on_event(defines.events.on_entity_logistic_slot_changed, on_entity_logistic_slot_changed)
+script.on_event(defines.events.on_lua_shortcut, on_lua_shortcut)
+script.on_event("logistics_requests_sorted-hotkey", on_lua_shortcut)
--- scripts/inventory.lua
+++ scripts/inventory.lua
@@ -1,61 +1,103 @@
-local Inventory = {}
+local inventory_sort = {}
 
-Inventory.SortEntityInventory = function(entity)
+--- Use Inventory.sort_and_merge() to sort the logistic sections
+--- @param entity LuaEntity
+inventory_sort.sort_logistic_sections = function(entity)
     if entity == nil then
         return
     end
-    
-    --record request items into inventory, counts into counts
-    local inventory = game.create_inventory(entity.request_slot_count)
-    local counts = {}
 
-    --record and remove all old requests
-    for i = 1, entity.request_slot_count do
-        local oldValue = Inventory.GetSlot(entity, i)
-        if oldValue.name ~= nil then
-            inventory.insert({name=oldValue.name})
-            counts[oldValue.name] = oldValue
-        end
-        Inventory.ClearSlot(entity, i)
+    local logistic_sections = entity.get_logistic_sections()
+
+    if logistic_sections == nil then
+        return
     end
 
-    inventory.sort_and_merge()
+    local test_section = nil
+    local sections = {}
+
+    for _, section in pairs(logistic_sections.sections) do
+        if section.is_manual then
+            if (section.group == "TEST") then
+                test_section = section
+            else
+                sections[#sections] = section
+            end
+        end
+    end
+
+    if test_section then
+        for index, _ in pairs(test_section.filters) do
+            test_section.clear_slot(index)
+        end
+    end
+
+    -- Record each request section's filters into an inventory and save the filters into a table
+    for _, section in pairs(sections) do
+        if section.is_manual then
+            local inventory = game.create_inventory(section.filters_count)
+            local filters = {}
+
+            for filter_index, filter in pairs(section.filters) do
+                if filter.value and filter.value.name then
+                    inventory.insert({ name = filter.value.name })
+
+                    if not filters[filter.value.name] then
+                        filters[filter.value.name] = {}
+                    end
+
+                    -- TODO: Sort filters by rarity and comparator
+                    filters[filter.value.name][filter_index] = filter
+                end
+            end
+
+            log(serpent.block(filters))
+            inventory.sort_and_merge()
+            -- TODO: Continue if inventory sorting changed nothing
+
+            -- for index, _ in pairs(section.filters) do
+            --     section.clear_slot(index)
+            -- end
+
+            local section_index = 1
+
+            for _, item in pairs(inventory.get_contents()) do
+                local filter = filters[item.name]
+                for _, f in pairs(filter) do
+                    test_section.set_slot(section_index, f)
+                    section_index = section_index + 1
+                end
+            end
+        end
+    end
+
+    ----record request items into inventory, counts into counts
+    --local inventory = game.create_inventory(entity.request_slot_count)
+    --local counts = {}
+    --
+    ----record and remove all old requests
+    --for i = 1, entity.request_slot_count do
+    --    local old_value = inventory.get_slot(entity, i)
+    --    if old_value.name ~= nil then
+    --        inventory.insert({ name = old_value.name })
+    --        counts[old_value.name] = old_value
+    --    end
+    --    inventory.clear_slot(entity, i)
+    --end
+
+
+    -- inventory.sort_and_merge()
 
     --put them back, now in sorted order
-    local slotIndex = 0
-    for i = 1,#inventory - inventory.count_empty_stacks() do
-        local invitem = inventory[i]
-        slotIndex = slotIndex + 1
-        Inventory.SetSlot(entity, slotIndex, {name = invitem.name, min = counts[invitem.name].min, max = counts[invitem.name].max})
-    end
+    -- local slotIndex = 0
+    -- for i = 1, #inventory - inventory.count_empty_stacks() do
+    --     local invitem = inventory[i]
+    --     slotIndex = slotIndex + 1
+    --     inventory.set_slot(entity, slotIndex,
+    --             { name = invitem.name, min = counts[invitem.name].min, max = counts[invitem.name].max })
+    -- end
 
-    --cleanup
-    inventory.destroy()
+    -- inventory.destroy()
 end
 
--- helpers to abstract between player and vehicles with logistic slots
-Inventory.GetSlot = function(entity, index)
-    if entity.type == "character" then
-        return entity.get_personal_logistic_slot(index)
-    else
-        return entity.get_vehicle_logistic_slot(index)
-    end
-end
-
-Inventory.ClearSlot = function(entity, index)
-    if entity.type == "character" then
-        entity.clear_personal_logistic_slot(index)
-    else
-        entity.clear_vehicle_logistic_slot(index)
-    end
-end
-
-Inventory.SetSlot = function(entity, index, val)
-    if entity.type == "character" then
-        entity.set_personal_logistic_slot(index, val)
-    else
-        entity.set_vehicle_logistic_slot(index, val)
-    end
-end
-
-return Inventory
+return inventory_sort
