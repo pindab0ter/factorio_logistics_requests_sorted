1ed1187b11f9dcb19f00f7b9675a740cfddcedc3
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-12-06 13:51:51 +0100

    Apply the sorting to the sections

--- scripts/inventory.lua
+++ scripts/inventory.lua
@@ -32,7 +32,53 @@ local logistic_filter_comparator = function(a, b)
         or comparator_order[a.value.comparator] < comparator_order[b.value.comparator]
 end
 
---- Use Inventory.sort_and_merge() to sort the logistic sections
+--- Sorts the filter item order using a `LuaInventory` and then sorts the filters with the same items by quality and
+--- comparator.
+--- TODO: Find out why sometomes the sorting doesn't go well the first time.
+--- @param section LuaLogisticSection
+local function sort_section(section)
+    local inventory = game.create_inventory(section.filters_count)
+    --- @type table<string, table<number, LogisticFilter>>
+    local filter_sets = {}
+
+    local i = 1
+    for _, filter in pairs(section.filters) do
+        if filter.value and filter.value.name then
+            inventory.insert({ name = filter.value.name })
+
+            if not filter_sets[filter.value.name] then
+                filter_sets[filter.value.name] = {}
+            end
+
+            filter_sets[filter.value.name][i] = filter
+            i = i + 1
+        end
+    end
+
+    -- TODO: Skip further processing if the inventory order has not changed?
+    inventory.sort_and_merge()
+
+    -- Clear all filters from the section so we can re-add them in the correct order without running into conflicts
+    for index, _ in pairs(section.filters) do
+        section.clear_slot(index)
+    end
+
+    -- Manually track where we should add the next filter, as each item can have multiple filters
+    local section_index = 1
+
+    -- Insert the filters back in, in the order of the sorted inventory
+    for _, item in pairs(inventory.get_contents()) do
+        local filters = filter_sets[item.name]
+        table.sort(filters, logistic_filter_comparator)
+
+        for _, filter in pairs(filters) do
+            section.set_slot(section_index, filter)
+            section_index = section_index + 1
+        end
+    end
+    inventory.destroy()
+end
+
 --- @param entity LuaEntity
 inventory_sort.sort_logistic_sections = function(entity)
     if entity == nil or not entity.valid then
@@ -45,96 +91,11 @@ inventory_sort.sort_logistic_sections = function(entity)
         return
     end
 
-    local test_section = nil
-    local sections = {}
-
     for _, section in pairs(logistic_sections.sections) do
         if section.is_manual then
-            if (section.group == "TEST") then
-                test_section = section
-            else
-                sections[#sections] = section
-            end
+            sort_section(section)
         end
     end
-
-    if test_section then
-        for index, _ in pairs(test_section.filters) do
-            test_section.clear_slot(index)
-        end
-    end
-
-    -- Record each request section's filters into an inventory and save the filters into a table
-    for _, section in pairs(sections) do
-        -- TODO: Refactor into `sort_section`
-        if section.is_manual then
-            local inventory = game.create_inventory(section.filters_count)
-            local filter_sets = {}
-
-            local i = 1
-            for _, filter in pairs(section.filters) do
-                if filter.value and filter.value.name then
-                    inventory.insert({ name = filter.value.name })
-
-                    if not filter_sets[filter.value.name] then
-                        filter_sets[filter.value.name] = {}
-                    end
-
-                    filter_sets[filter.value.name][i] = filter
-                    i = i + 1
-                end
-            end
-
-            inventory.sort_and_merge()
-            -- TODO: Continue if inventory sorting changed nothing
-
-            -- for index, _ in pairs(section.filters) do
-            --     section.clear_slot(index)
-            -- end
-
-            local section_index = 1
-
-            for _, item in pairs(inventory.get_contents()) do
-                local filters = filter_sets[item.name]
-                table.sort(filters, logistic_filter_comparator)
-
-                print("Filters: ", serpent.block(filters))
-
-                for _, filter in pairs(filters) do
-                    test_section.set_slot(section_index, filter)
-                    section_index = section_index + 1
-                end
-            end
-        end
-    end
-
-    ----record request items into inventory, counts into counts
-    --local inventory = game.create_inventory(entity.request_slot_count)
-    --local counts = {}
-    --
-    ----record and remove all old requests
-    --for i = 1, entity.request_slot_count do
-    --    local old_value = inventory.get_slot(entity, i)
-    --    if old_value.name ~= nil then
-    --        inventory.insert({ name = old_value.name })
-    --        counts[old_value.name] = old_value
-    --    end
-    --    inventory.clear_slot(entity, i)
-    --end
-
-
-    -- inventory.sort_and_merge()
-
-    --put them back, now in sorted order
-    -- local slotIndex = 0
-    -- for i = 1, #inventory - inventory.count_empty_stacks() do
-    --     local invitem = inventory[i]
-    --     slotIndex = slotIndex + 1
-    --     inventory.set_slot(entity, slotIndex,
-    --             { name = invitem.name, min = counts[invitem.name].min, max = counts[invitem.name].max })
-    -- end
-
-    -- inventory.destroy()
 end
 
 return inventory_sort
